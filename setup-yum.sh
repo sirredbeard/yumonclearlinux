9108d4ae7e6df9d3d6dea325a97a826c33ab1e57
Hayden <33820650+sirredbeard@users.noreply.github.com>
2017-12-21 18:59:33 -0500

    Documented script

--- setup-yum.sh
+++ setup-yum.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo swupd add-bundle os-clr-on-clr
+sudo swupd add-bundle os-clr-on-clr       #install bundle containing yum
 
-sudo cp /etc/yum.conf /etc/yum.conf.old
-sudo rm /etc/yum.conf
+sudo cp /etc/yum.conf /etc/yum.conf.old   #if for some reason you have something there, we'll back it up
+sudo rm /etc/yum.conf                     #clear the way for our file
 
-sudo cp yum.conf /etc/yumd.conf
+sudo cp yum.conf /etc/yumd.conf           #copy the yum.conf from this repo
